d693e86882523cc01e7c36ff2e75c26b685aa397
bhuduttsharma <76999431+bhuduttsharma@users.noreply.github.com>
2024-04-02 16:37:09 +0530

    test

---
+++ angular-curd/src/main/java/com/example/demo/AngularCurdApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularCurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularCurdApplication.class, args);
+	}
+
+}
